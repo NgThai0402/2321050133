49f28adabf5d8aacf58186c8ded7a6e3f923b474
NgThai0402 <nguyenthai.it1@gmail.com>
2025-11-14 11:46:34 +0700

    Add files via upload

---
+++ Bài 15.sql
@@ -0,0 +1,51 @@
+CREATE DATABASE IF NOT EXISTS qly_fpt;
+USE qly_fpt;
+
+CREATE TABLE  nguoi_dung(
+	id INT,
+	tenDangNhap  VARCHAR(30),
+	matKhau VARCHAR(50),
+	hoTen VARCHAR(50),
+	email VARCHAR(50),
+	sdt VARCHAR(10),
+	ngaySinh DATETIME,
+	quyen_ID INT for
+)
+
+CREATE TABLE quyen(
+	quyen_ID INT PRIMARY KEY,
+	tenQuyen VARCHAR(20)
+	
+CREATE TABLE phim(
+	id INT PRIMARY KEY,
+	namPhatHanh DATE,
+	Ảnh phim VARCHAR,
+	daoDien_ID INT,
+	dienVien VARCHAR(30)
+	trailer varchar
+	sotap INT	
+	mota TEXT		
+CREATE TABLE phim_dv(
+id INT 
+phimID INT
+dienvienid INT
+
+
+CREATE TABLE theLoai(
+	id INT PRIMARY KEY,
+	tenTheLoai VARCHAR(50),
+	phimid
+
+4. Tập phim
+	id int
+	so_tap int
+	tieu_de varchar
+	phim_id
+	thoiluong float
+	trailer VARCHAR 
+	- 
+
+5. Quốc gia
+id INT 
+	- Tên châu lục
+		+ Tên quốc gia
